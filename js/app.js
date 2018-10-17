ba65e7e7ec37619fa5e8a90cdf9db43ffabfb5a5
Joyce Liao <joyliao07@gmail.com>
2018-10-17 15:22:25 -0700

    select list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
 'use strict';
 
+function Horns (creature) {
+  this.title = creature.title;
+  this.picture = creature.image_url;
+  this.description = creature.description;
+  this.keyword = creature.keyword;
+  this.hornNum = creature.horns;
+  this.render();
+}
+
+
+Horns.prototype.render = function () {
+  let $newSection = $('#horn-template').html();
+  let compiled = Handlebars.compile($newSection);
+  $('#horn-display').append(compiled(this));
+}
+
+
+Horns.allhorn = [];
+Horns.keyword = [];
+
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+
+Horns.selectList = function () {
+  Horns.allhorn.forEach( horn => {
+    Horns.keyword.push(horn.keyword);
+  });
+  Horns.keyword = Horns.keyword.filter(onlyUnique).sort();
+  Horns.selectRender();
+}
+
+
+Horns.selectRender = function () {
+  Horns.keyword.forEach( key => {
+    console.log('key is: ', key);
+    let $newSelect = $('#select-template').html();
+    let compiled = Handlebars.compile($newSelect);
+    $('#select-display').append(compiled(key));
+  });
+}
 
 
 
@@ -7,18 +50,15 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+Horns.readJson = function (){
+  $.get('./data/page-1.json', 'json')
+    .then( data => {
+      data.forEach( each => {
+        Horns.allhorn.push(new Horns(each));
+      });
+      Horns.selectList();
+    });
+}
 
 
 
@@ -30,5 +70,5 @@
 
 
 $(document).ready(function () {
-  console.log('j-query ready');
+  Horns.readJson();
 })
